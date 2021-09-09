c6c7ae838a19da55fe95bcbac82d1b7a681a03f7
Meruem117 <1572677319@qq.com>
2021-09-09 23:35:26 +0800

    add ejs for express

--- src/express-server/app.js
+++ src/express-server/app.js
@@ -2,24 +2,13 @@ const express = require('express')
 
 const app = express()
 
+//* base
 app.get('/', (request, response) => {
     response.send('首页')
 })
 
 app.get('/getTest', (request, response) => {
     response.send('getTest')
-})
-
-app.post('/postTest', (request, response) => {
-    response.send('postTest')
-})
-
-app.put('/putTest', (request, response) => {
-    response.send('putTest')
-})
-
-app.delete('/deleteTest', (request, response) => {
-    response.send('deleteTest')
 })
 
 app.get('/getTest/test', (request, response) => {
@@ -35,6 +24,26 @@ app.get('/product', (request, response) => {
     const query = request.query
     console.log(query)
     response.send('product')
+})
+
+app.post('/postTest', (request, response) => {
+    response.send('postTest')
+})
+
+app.put('/putTest', (request, response) => {
+    response.send('putTest')
+})
+
+app.delete('/deleteTest', (request, response) => {
+    response.send('deleteTest')
+})
+
+//* ejs
+app.set('view engine', 'ejs')
+
+app.get('/home', (request, response) => {
+    const message = 'ejs test'
+    response.render('home', { message: message })
 })
 
 app.listen(3333)
