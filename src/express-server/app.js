226cacc786e6b66a885cec9f28ae3809b56ffadc
Meruem117 <1572677319@qq.com>
2021-09-09 23:12:51 +0800

    add some routes

--- src/express-server/app.js
+++ src/express-server/app.js
@@ -6,6 +6,37 @@ app.get('/', (request, response) => {
     response.send('首页')
 })
 
+app.get('/getTest', (request, response) => {
+    response.send('getTest')
+})
+
+app.post('/postTest', (request, response) => {
+    response.send('postTest')
+})
+
+app.put('/putTest', (request, response) => {
+    response.send('putTest')
+})
+
+app.delete('/deleteTest', (request, response) => {
+    response.send('deleteTest')
+})
+
+app.get('/getTest/test', (request, response) => {
+    response.send('getTest/test')
+})
+
+app.get('/getTest/:id', (request, response) => {
+    const id = request.params['id']
+    response.send('id = ' + id)
+})
+
+app.get('/product', (request, response) => {
+    const query = request.query
+    console.log(query)
+    response.send('product')
+})
+
 app.listen(3333)
 
 console.log('Server running at http://localhost:3333/')
